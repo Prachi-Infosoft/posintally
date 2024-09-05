5213c28a840b4d7fc77e61565d4bc57f856a8958
Tushar Deshmukh <tdlwala@gmail.com>
2024-09-05 09:50:37 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const faqQuestions = document.querySelectorAll(".faq-question");
+
+    faqQuestions.forEach(question => {
+        question.addEventListener("click", function() {
+            const answer = this.nextElementSibling;
+
+            // Toggle the FAQ answer visibility
+            if (answer.style.display === "block") {
+                answer.style.display = "none";
+            } else {
+                answer.style.display = "block";
+            }
+        });
+    });
+});
